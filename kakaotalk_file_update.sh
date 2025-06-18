f52c43c7cb312dbd9fe98ceb0a8f7fe3175dfe94
small-goliath <meang617@gmail.com>
2025-06-18 10:50:02 +0900

    chore(kakaotalk_file_update.sh): 파일 업데이트 시 로그 메시지 추가
    파일 생성 및 수정 시 로그 메시지와 날짜를 출력하여
    스크립트 실행 시점과 상태를 확인할 수 있도록 개선.

--- kakaotalk_file_update.sh
+++ kakaotalk_file_update.sh
@@ -6,6 +6,8 @@ source .venv/bin/activate
 inotifywait -m -e create,modify "${WATCH_FILE}" | while read FILE
 do
     pip install -r requirements.txt
+    echo "kakaotalk file update!!!"
+    date
     python3 -m app.batch.kakaotalk_active_verify
     python3 -m app.batch.kakaotalk_active
 done
